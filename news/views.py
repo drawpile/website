10fc6a6020ec30392097edc22dee068a1c77a7a6
Calle Laakkonen <laakkonenc@gmail.com>
2018-02-14 17:50:54 +0200

    Allow admins to see hidden news posts

--- news/views.py
+++ news/views.py
@@ -12,5 +12,10 @@ class NewsListView(ListView):
 
 class NewsDetailView(DetailView):
     def get_queryset(self):
-        return models.Post.objects.visible()
+        qs = models.Post.objects
+        if not self.request.user.has_perm('news.change_post'):
+            qs = qs.visible()
+        else:
+            qs = qs.all()
+        return qs
 
