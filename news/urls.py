a3b1dae33f25ce57656e988e49f55ee2a56df187
Calle Laakkonen <laakkonenc@gmail.com>
2019-12-20 18:46:38 +0200

    Add Atom feed

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.NewsListView.as_view(), name="list"),
+    url(r'^feed.atom$', views.NewsFeed(), name="atom-feed"),
     url(r'^(?P<slug>[-.\w]+)/$', views.NewsDetailView.as_view(), name='detail'),
 ]
 
--- news/views.py
+++ news/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
 
 from . import models
+
+from commonmark import commonmark
+
 
 class NewsListView(ListView):
     paginate_by=5
@@ -19,3 +24,23 @@ class NewsDetailView(DetailView):
             qs = qs.all()
         return qs
 
+
+class NewsFeed(Feed):
+    feed_type = Atom1Feed
+    title = "Drawpile.net news"
+    link = '/news/'
+
+    def items(self):
+        return models.Post.objects.visible()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_author_name(self):
+        return "Drawpile.net"
+
+    def item_description(self, item):
+        return commonmark(item.intro)
+
+    def item_pubdate(self, item):
+        return item.publish
