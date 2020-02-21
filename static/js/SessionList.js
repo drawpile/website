ee21c128053d6c1ae38b0148ac65c96f862f96b0
Calle Laakkonen <laakkonenc@gmail.com>
2020-02-21 20:31:57 +0200

    Fix sessionlist widget

--- static/js/SessionList.js
+++ static/js/SessionList.js
@@ -1,7 +1,7 @@
 // MIT License
-//
+// 
 // Copyright (c) 2019 Calle Laakkonen
-//
+// 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
@@ -11,7 +11,7 @@
 // 
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -63,7 +63,7 @@ const listingTable = () => el('div', {class: 'root'},
 );
 
 const uptime = (ts, now) => {
-	const started = Date.parse(ts.replace(' ', 'T') + "+00:00");
+	const started = Date.parse(ts);
 	const uptime = (now - started) / (1000*60);
 	const hours = Math.floor(uptime / 60);
 	const minutes = Math.floor(uptime - hours * 60);
