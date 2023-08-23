911240056e5ce1a90188c39d0ef1f5d93c62e15b
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-08-23 17:24:14 +0200

    Set username cookie along with session
    So that docs.drawpile.net can read it and show it in the top-right
    corner, rather than punting to just saying "Account" there.

---
+++ dpusers/middleware.py
@@ -0,0 +1,38 @@
+import time
+from django.utils.http import http_date
+
+class UsernameCookieMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        try:
+            empty = request.session.is_empty()
+        except AttributeError:
+            return response
+
+        username = request.user.username
+        has_cookie = 'username' in request.COOKIES
+        if username:
+            if not has_cookie:
+                if request.session.get_expire_at_browser_close():
+                    max_age = None
+                    expires = None
+                else:
+                    max_age = request.session.get_expiry_age()
+                    expires_time = time.time() + max_age
+                    expires = http_date(expires_time)
+                response.set_cookie(
+                    'username',
+                    username,
+                    max_age=max_age,
+                    expires=expires,
+                    httponly=False,
+                    samesite='Lax',
+                )
+        elif has_cookie:
+            response.delete_cookie('username')
+
+        return response
