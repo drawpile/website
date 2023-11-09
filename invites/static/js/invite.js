49ac0843ceb47f0c1fb573c3398104402695e134
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-11-09 05:47:34 +0100

    Don't mangle invite link when refreshing page
    The input field remembers its last value, so refreshing the page causes
    the password to get appended repeatedly. Now the fact that we appended
    the password gets stored into a hidden input field, which is checked to
    see if we already did it.

--- invites/static/js/invite.js
+++ invites/static/js/invite.js
@@ -2,13 +2,17 @@
     const title = document.getElementById('invite-link-title');
     const input = document.getElementById('invite-link-input');
     const button = document.getElementById('invite-link-copy-button');
+    const checked = document.getElementById('invite-link-checked');
 
-    const hash = window.location.hash;
     let link = input.value;
-    if(hash && hash.length > 1) {
-        link += `?p=${hash.substring(1)}`;
+    if(checked.value === '') {
+        const hash = window.location.hash;
+        if(hash && hash.length > 1) {
+            link += `?p=${hash.substring(1)}`;
+        }
+        input.value = link;
+        checked.value = '1';
     }
-    input.value = link;
 
     button.addEventListener('click', () => {
         input.select();
