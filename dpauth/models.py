efead134d8eb82e7ad16b5073f59080f1244391e
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-02-10 22:47:01 +0100

    Extend normalized name field to 255 characters max
    Because normalization may increase the length of this field, due to
    case-folding and other Unicode fun.

--- dpauth/models.py
+++ dpauth/models.py
@@ -47,7 +47,7 @@ class Username(models.Model):
         )]
     )
     normalized_name = models.CharField(
-        max_length=22,
+        max_length=255,
         unique=True,
         blank=True, # this field gets autoassigned in the clean() method
         help_text="Normalized username for querying"
