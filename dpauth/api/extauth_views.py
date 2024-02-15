407fd0d0c716119e6fc488cd420e545b38785ca3
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-02-15 18:24:14 +0100

    Auto-format extauth_views.py

--- dpauth/api/extauth_views.py
+++ dpauth/api/extauth_views.py
@@ -18,47 +18,40 @@ class ExtAuthView(APIView):
     permission_classes = (AllowAny,)
 
     def post(self, request, format=None):
-        if 'password' in request.data:
+        if "password" in request.data:
             return Response(self.__handle_auth_attempt(request))
         else:
             return Response(self.__handle_account_query(request))
 
     def __handle_auth_attempt(self, request):
-        """A real authentication attempt.
-        """
+        """A real authentication attempt."""
         serializer = AuthAttemptSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
 
-        username = Username.getByName(data['username'])
+        username = Username.getByName(data["username"])
         if username and not username.user.is_active:
-            return {
-                "status": "banned"
-            }
+            return {"status": "banned"}
 
-        if not username or not check_password(data['password'], username.user.password):
-            return {
-                "status": "badpass"
-            }
+        if not username or not check_password(data["password"], username.user.password):
+            return {"status": "badpass"}
 
-        group_membership = _group_membership_function()(username, data.get('group'))
+        group_membership = _group_membership_function()(username, data.get("group"))
 
         if group_membership is None:
-            return {
-                "status": "outgroup"
-            }
-   
+            return {"status": "outgroup"}
+
         username.user.last_login = timezone.now()
-        username.user.save(update_fields=('last_login',))
+        username.user.save(update_fields=("last_login",))
 
         return {
             "status": "auth",
             "token": username.make_login_token(
-                data['nonce'],
-                flags=group_membership['flags'],
-                group=data.get('group'),
-                avatar=data['avatar']
-            )
+                data["nonce"],
+                flags=group_membership["flags"],
+                group=data.get("group"),
+                avatar=data["avatar"],
+            ),
         }
 
     def __handle_account_query(self, request):
@@ -72,14 +65,12 @@ class ExtAuthView(APIView):
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
 
-        user = Username.getByName(data['username'])
+        user = Username.getByName(data["username"])
 
         if user and not user.user.is_active:
-            return {
-                "status": "banned"
-            }
+            return {"status": "banned"}
 
-        if extauth_settings['GUEST_LOGINS']:
+        if extauth_settings["GUEST_LOGINS"]:
             return {
                 "status": "auth" if user else "guest",
             }
@@ -91,28 +82,30 @@ class ExtAuthView(APIView):
                 "status": "auth",
             }
 
+
 class GroupMembershipImportError(Exception):
     pass
 
+
 def _group_membership_function():
-    impl = extauth_settings['GROUP_IMPL']
+    impl = extauth_settings["GROUP_IMPL"]
 
     if isinstance(impl, str):
-        modname, funcname = impl.rsplit('.', 1)
+        modname, funcname = impl.rsplit(".", 1)
         try:
             mod = import_module(modname)
         except ImportError:
             raise GroupMembershipImportError(modname, "no such module")
 
         func = getattr(mod, funcname, None)
-        if not hasattr(func, '__call__'):
-            raise GroupMembershipImportError(impl,"does not point to a function!")
+        if not hasattr(func, "__call__"):
+            raise GroupMembershipImportError(impl, "does not point to a function!")
 
         # Cache the resolved function
-        extauth_settings['GROUP_IMPL'] = func
+        extauth_settings["GROUP_IMPL"] = func
         return func
 
-    elif not hasattr(impl, '__call__'):
+    elif not hasattr(impl, "__call__"):
         raise GroupMembershipImportError(repr(impl), "not an import path or a function")
 
     return impl
