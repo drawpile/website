863faa2361354aae85c7d5df3a6be9f0e88cc94b
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-03-19 12:46:26 +0100

    Auto-format templatepages views

--- templatepages/views.py
+++ templatepages/views.py
@@ -3,6 +3,7 @@ from django.template.response import TemplateResponse
 from django.template.exceptions import TemplateDoesNotExist
 from django.http import Http404
 
+
 class TemplatePageResponse(TemplateResponse):
     def resolve_template(self, template):
         try:
@@ -10,22 +11,25 @@ class TemplatePageResponse(TemplateResponse):
         except TemplateDoesNotExist:
             raise Http404
 
+
 class TemplatePageView(TemplateView):
     response_class = TemplatePageResponse
-    template_root = 'pages/'
+    template_root = "pages/"
 
     def get_template_names(self):
-        path = self.kwargs['path']
+        path = self.kwargs["path"]
 
-        if path and path[-1] == '/':
+        if path and path[-1] == "/":
             path = path[:-1]
 
         if not path:
             return (self.template_root + "index.html",)
 
         for c in path:
-            if not c.isalnum() and c not in '/-.':
+            if not c.isalnum() and c not in "/-.":
                 raise Http404
 
-        return (self.template_root + path + '.html', self.template_root + path + '/index.html')
-
+        return (
+            self.template_root + path + ".html",
+            self.template_root + path + "/index.html",
+        )
