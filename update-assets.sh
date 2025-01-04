f991a08ef4a2fe34d508aa78e2baa25edd8d09a5
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-01-04 20:54:16 +0100

    Rejig update-assets.sh to be prettier

--- update-assets.sh
+++ update-assets.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-
-for img in assets/images/*
-do
-	./manage.py images add "$img"
+#!/bin/sh
+for img in assets/images/*; do
+    set -x
+    ./manage.py images add "$img"
+    set +x
 done
 
-for post in assets/news/*
-do
-	./manage.py news import "$post"
+for post in assets/news/*; do
+    set -x
+    ./manage.py news import "$post"
+    set +x
 done
 
-./manage.py templatevar VERSION 2.1.20
-./manage.py templatevar BETAVERSION 2.2.0-beta.7
-
+./manage.py templatevar VERSION 2.2.1
+./manage.py templatevar BETAVERSION 2.2.2-beta.4
