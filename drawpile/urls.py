2bc40ff4dd9dfe4159febc4916d905170c2e797b
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-01-21 03:07:31 +0100

    Add pubrules shortcut url

--- drawpile/urls.py
+++ drawpile/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('discord/', RedirectView.as_view(url='https://discord.gg/M3yyMpC', permanent=True)),
     path('matrix/', RedirectView.as_view(url='https://matrix.to/#/#drawpile:matrix.org', permanent=True)),
     path('localhosthelp/', RedirectView.as_view(url='https://docs.drawpile.net/help/common/hosting#hosting-on-your-computer', permanent=True)),
+    path('pubrules/', RedirectView.as_view(url='/communities/drawpile.net/ccg/', permanent=True)),
     path('sharedarraybufferhelp/', RedirectView.as_view(url='https://docs.drawpile.net/help/tech/browser#sharedarraybuffer-issues', permanent=True)),
     path('lgm/', TemplateView.as_view(template_name='lgm.html')),
     path('namecheckbot/', TemplateView.as_view(template_name='namecheckbot.html')),
